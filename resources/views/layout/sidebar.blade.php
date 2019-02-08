b888dcb8ca42e035bd7a09eaa4c1188cd255b4fa
Abdusalam Junianto <juniantoabdusalam@gmail.com>
2019-02-08 08:11:03 +0700

    add template

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,54 @@
+<div class="sidebar" data-color="purple" data-image="{{asset('template-admin/assets/img/sidebar-5.jpg')}}">
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a class="simple-text">
+                Ticketing
+            </a>
+        </div>
+
+        <ul class="nav">
+            <li class="active">
+                <a href="{{url('/kamar')}}">
+                    <i class="pe-7s-graph"></i>
+                    <p>Kamar</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/obat')}}">
+                    <i class="pe-7s-user"></i>
+                    <p>Obat</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/jenis_obat')}}">
+                    <i class="pe-7s-like"></i>
+                    <p>Jenis Obat</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/resep')}}">
+                    <i class="pe-7s-note2"></i>
+                    <p>Resep</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/pasien')}}">
+                    <i class="pe-7s-news-paper"></i>
+                    <p>Pasien</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/dokter')}}">
+                    <i class="pe-7s-science"></i>
+                    <p>Dokter</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/fasilitas')}}">
+                    <i class="pe-7s-map-marker"></i>
+                    <p>Fasilitas</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
